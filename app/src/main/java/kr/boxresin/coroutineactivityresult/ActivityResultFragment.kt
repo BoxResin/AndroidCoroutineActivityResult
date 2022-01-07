5aaf7e2844ac5fe41e6f0d272968afc357add713
Minsuk Eom <boxresin@gmail.com>
2022-01-07 22:34:19 +0900

    `ActivityResultViewModel`을 private 화

--- app/src/main/java/kr/boxresin/coroutineactivityresult/ActivityResultFragment.kt
+++ app/src/main/java/kr/boxresin/coroutineactivityresult/ActivityResultFragment.kt
@@ -1,15 +1,22 @@
 package kr.boxresin.coroutineactivityresult
 
 import android.content.Intent
+import android.util.SparseArray
 import androidx.core.util.containsKey
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 internal class ActivityResultFragment : Fragment() {
-    private val viewModel by lazy { ViewModelProvider(this)[ActivityResultViewModel::class.java] }
+    private val viewModel by lazy {
+        val factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T = modelClass.newInstance()
+        }
+        ViewModelProvider(this, factory)[InnerViewModel::class.java]
+    }
 
     suspend fun startActivityForResult(
         intent: Intent
@@ -29,5 +36,9 @@ internal class ActivityResultFragment : Fragment() {
         // 요청 코드에 해당하는 코루틴 재개
         this.viewModel.activityResultMap[requestCode]?.resume(ActivityResult(resultCode, data))
         this.viewModel.activityResultMap.remove(requestCode)
+    }
+
+    private class InnerViewModel : ViewModel() {
+        val activityResultMap = SparseArray<Continuation<ActivityResult>>()
     }
 }
--- app/src/main/java/kr/boxresin/coroutineactivityresult/ActivityResultViewModel.kt
+++
@@ -1,9 +0,0 @@
-package kr.boxresin.coroutineactivityresult
-
-import android.util.SparseArray
-import androidx.lifecycle.ViewModel
-import kotlin.coroutines.Continuation
-
-internal class ActivityResultViewModel : ViewModel() {
-    val activityResultMap = SparseArray<Continuation<ActivityResult>>()
-}
